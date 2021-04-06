da048356131f1824a4dd169c54eee2e899fd2a1c
mac <ajiboduemmanuel02@gmail.com>
2021-04-06 22:19:26 +0100

    add anotation

--- src/main/java/com/webspoons/churcheechatservice/repositorycache/MemberRepositoryCache.java
+++ src/main/java/com/webspoons/churcheechatservice/repositorycache/MemberRepositoryCache.java
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 @RequiredArgsConstructor
 public class MemberRepositoryCache implements MemberRepository {
 
